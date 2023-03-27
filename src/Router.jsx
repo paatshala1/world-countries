4736694753cb286a9e98cb8613c8e9ed53a7ee1e
paatshala1 <hugo.rodriguezz@icloud.com>
2023-03-27 15:38:06 -0600

    Testing router for: "/", login, home

---
+++ src/Router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Entrance from './components/Entrance/Entrance'
+import Home from './components/Home/Home'
+import LogInComponent from './components/LogIn/LogInComponent'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Entrance />,
+    children: [
+      {
+        path: 'login',
+        element: <LogInComponent />,
+      },
+      {
+        path: 'home',
+        element: <Home />,
+      },
+    ],
+  },
+])
+
+export default router

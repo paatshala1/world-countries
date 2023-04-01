9f892b42af3edfde5200d6f1c8379344942f9c1a
paatshala1 <hugo.rodriguezz@icloud.com>
2023-04-01 10:40:51 -0600

    Small redesign & message for not results

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-absolute-path */
 import SearchFormComponent from '../SearchForm/SearchFormComponent'
 import { useEffect } from 'react'
-import mapaMundi from '/images/mapaMundiAlphaChannel.png'
 import { NavLink } from 'react-router-dom'
 
 export default function Home() {
@@ -11,24 +10,24 @@ export default function Home() {
   const logStatus = JSON.parse(data)
 
   return (
-    <section className=' grid grid-cols-4 '>
+    <section className=' grid grid-cols-9 '>
       {!logStatus ? (
         <NavLink to='/' />
       ) : (
         <>
-          <h1 className='col-start-1 col-end-5 px-10 text-center text-indigo-600'>
+          <h1 className='col-start-2 col-end-9 px-10 text-center text-indigo-600'>
             You can search for any country by name, capital or contry code
           </h1>
-          <p className='col-start-1 col-end-5 text-center text-indigo-600'>
+          <p className='col-start-2 col-end-9 text-center text-indigo-600'>
             ...even with partial match
           </p>
-          <div className=' col-start-2 col-end-4 pt-6'>
+          {/* <div className=' col-start-2 col-end-4 pt-6'>
             <img
               src={mapaMundi}
               className='my-0 mx-auto  mt-7 w-1/4 rounded-full opacity-60'
               alt='Mapa Mundi'
             />
-          </div>
+          </div> */}
           <SearchFormComponent />
         </>
       )}
--- src/components/SearchForm/SearchFormComponent.jsx
+++ src/components/SearchForm/SearchFormComponent.jsx
@@ -4,9 +4,10 @@ import ButtonComponent from '../UI/Button/ButtonComponent'
 import MainContext from '../../store/main-context'
 import { useForm, useFormState } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 
 export default function SearchFormComponent() {
+  const [notResults, setNotResults] = useState(false)
   const ctx = useContext(MainContext)
   const navigate = useNavigate()
   const baseURL = 'https://restcountries.com/v3.1/'
@@ -27,8 +28,8 @@ export default function SearchFormComponent() {
     setValue,
   } = useForm({
     defaultValues: {
-      country: '',
-      capital: '',
+      // country: '',
+      criteria: 'name',
       searcher: '',
     },
   })
@@ -48,9 +49,12 @@ export default function SearchFormComponent() {
     const response = await httpCountriesRequest(options)
     if (!response.data) {
       ctx.onSetHttpError(response)
-      navigate('/error')
+      setNotResults(true)
+      // navigate('/error')
     } else {
       console.log(response)
+      setNotResults(false)
+      reset()
       // extractData(response.data)
       // sortCountries(requestedCountries)
       // setCountriesByCriteria(requestedCountries)
@@ -59,103 +63,121 @@ export default function SearchFormComponent() {
   }
 
   return (
-    <div className='col-start-2 col-end-4 mt-8 grid max-w-2xl  grid-cols-2 justify-self-center shadow-md shadow-stone-200'>
-      <section className=' bg-indigo-500 p-4'>
-        <div className=' mb-6 grid content-center'>
-          <LogoComponent />
-        </div>
-        <div>
-          <p className=' mb-1'>
-            There is several options to search for any country.
-          </p>
-          <p className=' mb-1'>
-            It is possible to search for its name or capital, to do that just go
-            to the right-hand section.
-          </p>
-          <p className=' mb-1'>
-            You can also navigate through the menu at top of page to start your
-            travel...it is a great idea to discover new countries.
-          </p>
-        </div>
-      </section>
-
-      <form
-        onSubmit={handleSubmit(onSubmitHandler)}
-        className=' grid gap-2 bg-zinc-50 p-8'
-      >
-        <ul className=' pl-4'>
-          <li className=' list-disc text-xs text-indigo-600'>
-            Select a criteria to search for.
-          </li>
-          <li className=' list-disc text-xs text-indigo-600'>
-            Type at least 3 characters.
-          </li>
-          <li className=' list-disc text-xs text-indigo-600'>Click search.</li>
-        </ul>
-
-        <div className='grid grid-cols-5 content-center '>
-          <div className='grid content-center'>
-            <input
-              type='radio'
-              name='criteria'
-              value='name'
-              id='country'
-              className=' my-radio-centered relative h-5 w-5'
-              {...register('criteria', {
-                required: { value: true, message: 'You must select an option' },
-              })}
-            />
+    <>
+      <div className='col-start-2 col-end-9 mt-8 grid  grid-cols-3  justify-self-center shadow-md shadow-stone-200'>
+        <section className=' bg-indigo-500 p-4'>
+          <div className=' mb-6 grid content-center'>
+            <LogoComponent />
           </div>
-          <label htmlFor='criteria' className=' text-2xl text-indigo-600 '>
-            Country
-          </label>
-        </div>
-
-        <div className='grid grid-cols-5 content-center '>
-          <div className='grid content-center'>
-            <input
-              type='radio'
-              name='criteria'
-              value='capital'
-              id='capital'
-              className=' my-radio-centered relative h-5 w-5'
-              {...register('criteria', {
-                required: { value: true, message: 'You must select an option' },
-              })}
-            />
+          <div>
+            <p className=' mb-1'>
+              There is several options to search for any country.
+            </p>
+            <p className=' mb-1'>
+              It is possible to search for its name or capital, to do that just
+              go to the right-hand section.
+            </p>
+            <p className=' mb-1'>
+              You can also navigate through the menu at top of page to start
+              your travel...it is a great idea to discover new countries.
+            </p>
           </div>
-          <label htmlFor='capital' className=' text-2xl text-indigo-600 '>
-            Capital
-          </label>
-        </div>
+        </section>
 
-        <input
-          type='input'
-          name='searcher'
-          id='searcher'
-          className='rounded-md bg-indigo-200 pl-2 text-indigo-600'
-          {...register('searcher', {
-            required: 'You must type any criteria to search for',
-            minLength: { value: 3, message: 'Min length 3' },
-          })}
-        />
-        {errors.searcher && (
-          <p className=' pl-2 text-xs text-red-700'>
-            {errors?.searcher.message}
-          </p>
+        <form
+          onSubmit={handleSubmit(onSubmitHandler)}
+          className=' grid gap-2 bg-zinc-50 p-8'
+        >
+          <ul className=' pl-4'>
+            <li className=' list-disc text-xs text-indigo-600'>
+              Select a criteria to search for.
+            </li>
+            <li className=' list-disc text-xs text-indigo-600'>
+              Type at least 3 characters.
+            </li>
+            <li className=' list-disc text-xs text-indigo-600'>
+              Click search.
+            </li>
+          </ul>
+
+          <div className='grid grid-cols-5 content-center '>
+            <div className='grid content-center'>
+              <input
+                type='radio'
+                name='criteria'
+                value='name'
+                id='country'
+                className=' my-radio-centered relative h-5 w-5'
+                {...register('criteria', {
+                  required: {
+                    value: true,
+                    message: 'You must select an option',
+                  },
+                })}
+              />
+            </div>
+            <label htmlFor='criteria' className=' text-2xl text-indigo-600 '>
+              Country
+            </label>
+          </div>
+
+          <div className='grid grid-cols-5 content-center '>
+            <div className='grid content-center'>
+              <input
+                type='radio'
+                name='criteria'
+                value='capital'
+                id='capital'
+                className=' my-radio-centered relative h-5 w-5'
+                {...register('criteria', {
+                  required: {
+                    value: true,
+                    message: 'You must select an option',
+                  },
+                })}
+              />
+            </div>
+            <label htmlFor='capital' className=' text-2xl text-indigo-600 '>
+              Capital
+            </label>
+          </div>
+
+          <input
+            type='input'
+            name='searcher'
+            id='searcher'
+            className='rounded-md bg-indigo-200 pl-2 text-indigo-600'
+            {...register('searcher', {
+              required: 'You must type any criteria to search for',
+              minLength: { value: 3, message: 'Min length 3' },
+            })}
+          />
+          {errors.searcher && (
+            <p className=' pl-2 text-xs text-red-700'>
+              {errors?.searcher.message}
+            </p>
+          )}
+          {errors?.criteria && (
+            <p className=' pl-2 text-xs text-red-700'>
+              {errors?.criteria?.message}
+            </p>
+          )}
+
+          <div className='mt-2 grid grid-cols-3 content-center'>
+            <div className=' col-start-1 col-end-4'>
+              <ButtonComponent label='Search' type='submit' />
+            </div>
+          </div>
+        </form>
+        {notResults && (
+          <div className=' p-4'>
+            <p className='  bg-orange-100 p-4 text-lg text-indigo-600 shadow-md'>
+              There is no results for these criterias, please try another
+              combination...or maybe a smaller text.
+            </p>
+          </div>
         )}
-        {errors?.criteria && (
-          <p className=' pl-2 text-xs text-red-700'>
-            {errors?.criteria?.message}
-          </p>
-        )}
-
-        <div className='mt-2 grid grid-cols-3 content-center'>
-          <div className=' col-start-1 col-end-4'>
-            <ButtonComponent label='Search' />
-          </div>
-        </div>
-      </form>
-    </div>
+      </div>
+    </>
   )
 }
